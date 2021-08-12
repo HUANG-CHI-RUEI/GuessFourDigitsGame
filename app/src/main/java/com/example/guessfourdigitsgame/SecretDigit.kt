875d537295f516087df5f77ddb70711137ed4573
HUANG-CHI-RUEI <pei4b066@gmail.com>
2021-08-12 21:11:57 +0800

    Validate 4 digits. But fail

--- app/src/main/java/com/example/guessfourdigitsgame/SecretDigit.kt
+++ app/src/main/java/com/example/guessfourdigitsgame/SecretDigit.kt
@@ -13,11 +13,32 @@ class SecretDigit {
     private val TAG = "Ray_Secret_Number"
 
     constructor() {
-        Log.d(TAG, "The secret number is: " + secret)
+        Log.d(TAG, "The secret number is: " + fourDigit)
+    }
+
+    fun getSecret():String {
+        return fourDigit
+    }
+
+    fun getCount(): Int{
+        return count
     }
 
     fun validate(guess: String): String {
+        var a: Int = 0
+        var b: Int = 0
+
         count++
-        return "4A0B"
+        for(i in 0..3) {
+            if (guess[i] in fourDigit){
+                if (guess[i] == fourDigit[i]) {
+                    a += 1
+                } else {
+                    b += 1
+                }
+            }
+
+        }
+        return "${a}A${b}B"
     }
 }
